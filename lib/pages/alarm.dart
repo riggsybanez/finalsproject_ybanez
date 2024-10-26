f628488a5fa698cfc5aec4f37a2cc69d48c2706b
riggsybanez <142960686+riggsybanez@users.noreply.github.com>
2024-10-26 23:39:44 +0800

    add button

--- lib/pages/alarm.dart
+++ lib/pages/alarm.dart
@@ -1,39 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/main.dart';
 
-class Alarm extends StatelessWidget {
+class Alarm extends StatefulWidget {
   const Alarm({super.key});
 
+  @override
+  _AlarmState createState() => _AlarmState();
+}
+
+class _AlarmState extends State<Alarm> {
   @override
   Widget build(BuildContext context) {
     return BaseScreen(
       selectedIndex: 0,
-      child: ListView.builder(
-        itemCount: 4,
-        itemBuilder: (context, index) {
-          return Card(
-            color: Colors.grey[300],
-            margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            child: ListTile(
-              leading: const Icon(Icons.alarm),
-              title: Text('Alarm ${index + 1}'),
-              subtitle: const Text('07:00 AM'),
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.edit),
-                    onPressed: () {},
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 4,
+              itemBuilder: (context, index) {
+                return Card(
+                  color: Colors.grey[300],
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  child: ListTile(
+                    leading: const Icon(Icons.alarm),
+                    title: Text('Alarm ${index + 1}'),
+                    subtitle: const Text('07:00 AM'),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.edit),
+                          onPressed: () {},
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.delete),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
                   ),
-                  IconButton(
-                    icon: const Icon(Icons.delete),
-                    onPressed: () {},
-                  ),
-                ],
-              ),
+                );
+              },
             ),
-          );
-        },
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              right: 16,
+              bottom: 16 + kBottomNavigationBarHeight,
+            ),
+            child: FloatingActionButton(
+              backgroundColor: Colors.grey[300],
+              onPressed: () {},
+              child: const Icon(Icons.add),
+            ),
+          ),
+        ],
       ),
     );
   }
